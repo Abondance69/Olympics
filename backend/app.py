82c7dbf157808b9b80b82deb64824d20da894461
Abondance <kazadiabondance@gmail.com>
2025-11-06 23:04:58 +0100

    fix port

--- backend/app.py
+++ backend/app.py
@@ -294,5 +294,5 @@ def overview():
 # =========================================================
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
